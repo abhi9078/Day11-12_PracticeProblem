f0ec638c15b4f41ffbd5fb386e07ea7459c654f7
Abhilash <abhilashmeher1234@gmail.com>
2022-03-27 12:37:13 +0530

    Added program file for StockManagement Problem

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockManagementProblem
+{
+    public class Stock
+    {
+        public String name { get; set; }
+        public int NumberOfShares { get; set; }
+        public int SharePrice { get; set; }
+        public int StockPrice { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Name: {0}\nPrice: {1}\nNumber of Shares: {2}", name, SharePrice, NumberOfShares);
+        }
+    }
+
+    public class StockPortfolio
+    {
+        public int grandTotal { get; set; }
+        public List<Stock> StockList;
+
+        public static void Main()
+        {
+            while (true)
+            {
+
+                Console.WriteLine("\nWelcome to Stock Management Program \n" +
+                    "Enter 1 to Add new Stock\n" +
+                    "Enter 2 for the Total Value of Stock\n");
+                int entered = int.Parse(Console.ReadLine());
+                StockManager sm = new StockManager();
+
+                switch (entered)
+                {
+                    case 1:
+                        sm.AddStock();
+                        break;
+                    case 2:
+                        sm.ValueOfStacks();
+                        break;
+            
+                    default:
+                        Console.WriteLine("Invalid Entry");
+                        break;
+                }
+            }
+        }
+    }
+
+}
---
+++ StockManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace StockManagementProblem
+{
+    public class StockManager
+    {
+        public static string path = @"D:\.Net\Assignment\Day11&12\StockManagementProblem\StockManagementProblem\Stock.json";
+        public void AddStock()
+        {
+            string jfile = File.ReadAllText(path);
+            StockPortfolio st;
+            if (jfile.Length < 1)
+            {
+                st = new StockPortfolio();
+                st.StockList = new List<Stock>();
+                st.grandTotal = 0;
+            }
+            else
+            {
+                st = JsonConvert.DeserializeObject<StockPortfolio>(jfile);
+            }
+
+            Stock s = new Stock();
+            Console.Write("Enter the new Stock Name:");
+            s.name = Console.ReadLine();
+            Console.Write("Enter the share Price For stock: ");
+            s.SharePrice = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Number Of Shares: ");
+            s.NumberOfShares = int.Parse(Console.ReadLine());
+            s.StockPrice = s.SharePrice * s.NumberOfShares;
+            st.grandTotal += s.StockPrice;
+
+            st.StockList.Add(s);
+
+            using (StreamWriter stream = File.CreateText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(stream, st);
+            }
+
+            Console.WriteLine("Added Successfully");
+        }
+
+        public void ValueOfStacks()
+        {
+            string jfile = File.ReadAllText(path);
+            StockPortfolio st;
+            if (jfile.Length < 1)
+            {
+                Console.WriteLine("there are no stocks");
+                return;
+            }
+            else
+            {
+                st = JsonConvert.DeserializeObject<StockPortfolio>(jfile);
+            }
+
+            Console.WriteLine("\nEnter 1 to Display Total Share value ");
+            Console.WriteLine("Enter 2 to display total sharePrice of particular stock");
+            int entered = int.Parse(Console.ReadLine());
+
+            switch (entered)
+            {
+                case 1:
+                    Console.WriteLine("total price of all stocks are: " + st.grandTotal);
+                    break;
+                case 2:
+                    Console.WriteLine("Name suggestions:");
+                    foreach (Stock s in st.StockList)
+                    {
+                        Console.Write("\"" + s.name + "\" ");
+                    }
+                    Console.Write("\nEnter the name: ");
+                    string name = Console.ReadLine();
+                    foreach (Stock s in st.StockList)
+                    {
+                        if (s.name.Equals(name))
+                        {
+                            Console.WriteLine("the Total Stock value of " + name + " is : " + s.StockPrice);
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry");
+                    break;
+            }
+
+
+
+        }
+    }
+}
